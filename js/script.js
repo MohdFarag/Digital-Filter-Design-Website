63c640a38c9addf6a1e8c5ec42dbf83b15402379
mohdfarag <mohamed.mahmoud02@eng-st.cu.edu.eg>
2022-05-25 01:32:44 +0200

    advanced plotter

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 
-// Initilize variable
+// Design 
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+}) 
 
+// Initilize variable
 // Z-plane plot
 var $zPlane = $("#z-plane");
 var canvasOffset = $zPlane.offset();
@@ -15,15 +19,116 @@ var $magnitudePlot = $("#magnitude-plot");
 // Phase plot
 var $phasePlot = $("#phase-plot");
 
-zeros = new Array // Zeros positions array
-poles = new Array // Poles positions array
+// Loaded data
+xloadedData = new Array
+yloadedData = new Array
+// Response data
+magResponse = new Array
+phaseResponse = new Array
+// Filtered data
+yfilteredData = new Array
 
+zeros = new Array // Zeros positions array
 var zerosNumber = 0;
+poles = new Array // Poles positions array
 var polesNumber = 0;
 
+//  Variables for mag and phase responses
+var Z = new Array(100);
+var freqAxis = new Array(100);
+
+for(let i = 0; i < 100; i++){
+    Z[i] = math.complex(Math.cos(Math.PI * (i/100)), Math.sin(Math.PI * (i/100)));
+    freqAxis[i] = Math.PI * (i/100);
+}
+
+function getCol(matrix, col){
+    var column = [];
+    for(var i=0; i<matrix.length; i++){
+       column.push(matrix[i][col]);
+    }
+    return column;
+}
+
+function linspace(startValue, stopValue, cardinality) {
+    var arr = [];
+    var step = (stopValue - startValue) / (cardinality - 1);
+    for (var i = 0; i < cardinality; i++) {
+      arr.push(startValue + (step * i));
+    }
+    return arr;
+}
+
+function inRange(number, start, end){
+    if (number >= start && number <= end) {
+        return true;
+    }
+    return false;
+}
+
+function performClick(elemId) {
+    var elem = document.getElementById(elemId);
+    if(elem && document.createEvent) {
+       var evt = document.createEvent("MouseEvents");
+       evt.initEvent("click", true, false);
+       elem.dispatchEvent(evt);
+    }
+    readCsv();
+ }
+
+function readCsv(){
+    const input = document.getElementById("files");
+    const fileReader = new FileReader()
+    fileReader.onload = (e) => {
+        loadData(e.target.result);
+    }
+
+    input.onchange = (e) => {
+        const [file] = e.target.files
+        fileReader.readAsBinaryString(file)
+    }
+}
+
+function loadData(results){
+
+    finalArray = new Array
+    resultsArray = results.split("\n");
+
+    for (let i = 0; i < resultsArray.length; i++) {
+        withoutSpace = resultsArray[i].replace('\r', '');
+        withoutDel =  withoutSpace.split(",")
+        finalRow = new Array
+        for (let i = 0; i < 2; i++) {
+            var element = parseFloat(withoutDel[i])
+            if (!isNaN(element)) {
+                finalRow.push(element);
+            }else{
+                break;
+            }
+        }
+        if (finalRow.length >= 2) {
+            finalArray.push(finalRow);
+        }
+    }    
+    xloadedData = getCol(finalArray, 0);
+    yloadedData = getCol(finalArray, 1);
+
+    plotLoadedData();
+}
 
 // Clear all points
 function clearAllPoints() {
+
+    for (let i = 0; i < zerosNumber; i++) {
+        var zeroDiv = document.getElementById('zero-' + i);
+        zeroDiv.remove();
+    }
+
+    for (let i = 0; i < polesNumber; i++) {
+        var poleDiv = document.getElementById('pole-' + i);
+        poleDiv.remove();
+    }
+
     poles = [];
     zeros = [];
     polesNumber = 0;
@@ -33,6 +138,12 @@ function clearAllPoints() {
 
 // Clear all zeros
 function clearZeros() {
+
+    for (let i = 0; i < zerosNumber; i++) {
+        var zeroDiv = document.getElementById('zero-' + i);
+        zeroDiv.remove();
+    }
+
     zeros = [];
     zerosNumber = 0;
     drawZplane(poles, zeros);
@@ -40,6 +151,12 @@ function clearZeros() {
 
 // Clear all poles
 function clearPoles() {
+
+    for (let i = 0; i < polesNumber; i++) {
+        var poleDiv = document.getElementById('pole-' + i);
+        poleDiv.remove();
+    }
+
     poles = [];
     polesNumber = 0;
     drawZplane(poles, zeros);
@@ -47,7 +164,6 @@ function clearPoles() {
 
 // Add new pole to Z plane
 function addNewPole() {
-    // // console.log("I'm here!");
     points = document.getElementById("poles");
     var div = document.createElement("div");
     div.id = 'pole-' + polesNumber;
@@ -69,34 +185,6 @@ function addNewZero() {
 
 // this var will hold the index of the selected point in zplane
 var selectedPoint = -1;
-
-// test if x,y is inside the bounding box of texts[textIndex]
-function pointHittest(x, y, textIndex) {
-    console.log([x,y]);
-    // console.log("poles.length is " + poles.length);
-    if(textIndex >= polesNumber){
-        // console.log("check some zero!");
-        // console.log(x, y, zeros[textIndex])
-        return insideElement(x, y, poles[textIndex - polesNumber], 0.02);
-    }
-
-    if(textIndex < polesNumber){
-        // console.log("check some pole!");
-        return insideElement(x, y, poles[textIndex], 0.05);
-    }
-}
-
-function insideElement(x, y, point, padd) {
-
-    xPoint = point[0]
-    yPoint = point[1]
-
-    if ((x > xPoint - padd) && (x <= xPoint + padd) && (y > yPoint - padd) && (y <= yPoint + padd)) {
-            return true
-    }
-
-    return false;
-}
 
 // handle mousedown events
 // iterate through texts[] and see if the user
@@ -197,12 +285,44 @@ function handleMouseClick(e) {
     drawZplane(poles, zeros);
 }
 
+// test if x,y is inside the bounding box of texts[textIndex]
+function pointHittest(x, y, textIndex) {
+    
+    // Edit
+    x -= 2.71 ;
+    y += 0.55;
+
+    console.log(x,y);
+
+    if(textIndex >= polesNumber){
+        return insideElement(x, y, zeros[textIndex - polesNumber]);
+    }
+
+    if(textIndex < polesNumber){
+        return insideElement(x, y, poles[textIndex]);
+    }
+}
+
+// Check if click inside the point
+function insideElement(x, y, point) {
+    
+    padd = 0.1
+    xPoint = point[0]
+    yPoint = point[1]
+
+    if ((x > xPoint - padd) && (x <= xPoint + padd) && (y > yPoint - padd) && (y <= yPoint + padd)) {
+            return true
+    }
+
+    return false;
+}
+
 // Draw Z plane
 function drawZplane(poles, zeros) {
 
 	var radius = 100;	// radius of unit circle
-	var pSize = 4;		// size of pole
-	var zSize = 4;      // size of zero
+	var pSize = 5;		// size of pole
+	var zSize = 5;      // size of zero
 
 	var zPlane = document.getElementById("z-plane"); // Get z Plane canvas element
 	var ctx = zPlane.getContext("2d");
@@ -255,28 +375,147 @@ function drawZplane(poles, zeros) {
 		ctx.stroke();
 	}
 
-
     drawResponse();
 }
 
 // Draw magnitude & phase response
 function drawResponse(){
+
+    magResponse = [];
+    phaseResponse = [];
+
+    for(let i = 0; i < 100; i++){
+
+        let magPoint = math.complex(1,0); // Initial value (1+0j)
+        let phasePoint = math.complex(1,0); // Initial value (1+0j)
+
+        // Calc. zeros
+        for(let j = 0; j < zeros.length; j++){
+            if(!(inRange(zeros[j][0], -0.01, 0.01) && inRange(zeros[j][1], -0.01, 0.01))){
+                let temp = math.subtract(Z[i], math.complex(zeros[j][0], zeros[j][1]));
+                magPoint *= temp.abs();
+                phasePoint *= temp.arg();
+            }else{
+                magPoint *= 1;
+                phasePoint *= 1;
+            }
+        }
+        
+        // Calc. poles
+        for(let j = 0; j < poles.length; j++){
+            if(!(inRange(poles[j][0], -0.01, 0.01) && inRange(poles[j][1], -0.01, 0.01))){                
+                let temp = math.subtract(Z[i], math.complex(poles[j][0], poles[j][1]));
+                magPoint /= temp.abs();
+                phasePoint /= temp.arg();
+            }else{
+                magPoint /= 1;
+                phasePoint /= 1;
+            }
+        }
+
+        magResponse.push(magPoint);
+        phaseResponse.push(phasePoint);
+    }
+    
     var magnitudeData = {
-        x: [1, 2, 3, 4],
-        y: [10, 15, 13, 17],
-        type: 'scatter'
+        x: freqAxis,
+        y: magResponse,
+        type: 'scatter',
+        name:'magnitude response',
+        line: {shape: 'spline'}
         };
-        
-    var phaseData = {
-        x: [1, 2, 3, 4],
-        y: [10, 15, 13, 17],
-        type: 'scatter'
+    
+    var layoutMagnitude = {
+        title: 'Magnitude response',
+        showlegend: true
     };
-        
+
+    var phaseData = {
+        x: freqAxis,
+        y: phaseResponse,
+        type: 'scatter',
+        name:'phase response',
+        line: {shape: 'spline'}
+    };
+
+    var layoutPhase = {
+        title: 'Phase response',
+        showlegend: true
+    };
+    
+    Plotly.newPlot('magnitude-plot', [magnitudeData], layoutMagnitude);
+    Plotly.newPlot('phase-plot', [phaseData], layoutPhase);
+
+    plotLoadedData();
+}
+
+function calcFilterData(){
+    coefSysY = new Array(poles.length);
+    coefSysX = new Array(zeros.length);
+
+    for (let i = 0; i < coefSysX.length; i++) {
+        coefSysX[i] = 0;
+    }
+
+    for (let i = 0; i < coefSysY.length; i++) {
+        coefSysY[i] = 0;
+    }
+
+    for (let i = 0; i < yfilteredData.length; i++) {
+        for (let j = 1; j < coefSysX.length; j++) {
+            if (i-j >= 0) {
+                yfilteredData[i] += yloadedData[i-j] * coefSysX[j];     
+            }
+        }
+    }
+
+    for (let i = 0; i < yfilteredData.length; i++) {
+        for (let j = 0; j < coefSysY.length; j++) {
+            if (i-j >= 0) {
+                yfilteredData[i] += yfilteredData[i-j] * coefSysY[j];     
+            }else{
+                yfilteredData[i] = 0;
+            }
+        }
+    }
+
+    return yfilteredData
+}
+
+// Draw loaded and filtered data
+function plotLoadedData(){
+
+    var originalData = {
+        x: xloadedData,
+        y: yloadedData,
+        type: 'scatter',
+        name:'Your Data',
+        line: {shape: 'spline'}
+        };
+    
+    var originalLayout = {
+        title: 'Your Data',
+        showlegend: true
+    };
+
+    /////////////////////////////////////////
+
+    yfilteredData = calcFilterData();
+    var filteredData = {
+        x: xloadedData,
+        y: yfilteredData,
+        type: 'scatter',
+        name:'Filtered Data',
+        line: {shape: 'spline'}
+    };
+
+    var filteredLayout = {
+        title: 'Filtered Data',
+        showlegend: true
+    };  
             
-    Plotly.newPlot('magnitude-plot', [magnitudeData]);
-    Plotly.newPlot('phase-plot', [phaseData]);
-        
+    Plotly.newPlot('original-signal-plot', [originalData], originalLayout);
+    Plotly.newPlot('filtered-signal-plot', [filteredData], filteredLayout);
 }
 
 // listen for mouse events
@@ -298,3 +537,5 @@ $zPlane.dblclick(function (e) {
 
 // Draw Z plane
 drawZplane(poles, zeros);
+// Draw Data
+plotLoadedData();
